000a81ab23e4ec848c189052e108af4c89e9c206
sander90 <sander.shan@yahoo.com>
2016-11-11 13:21:28 +0800

    chagne my hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def helloword
+  	render text: "Hello Word, This is my first web app"
+  	
+  end
 end
